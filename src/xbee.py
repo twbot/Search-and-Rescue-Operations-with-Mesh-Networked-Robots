43d2abde082df65f172bb727b6735d5d98d6cf68
Tristan Brodeur <brodeurtristan@gmail.com>
2018-07-02 09:54:58 -0700

    Checking RSSI

--- src/xbee.py
+++ src/xbee.py
@@ -2,14 +2,31 @@
 
 from digi.xbee.devices import XBeeDevice, ZigBeeDevice
 from digi.xbee.models.status import NetworkDiscoveryStatus
-from digi.xbee.packets.base import DictKeys
+from digi.xbee.packets.base import DictKeys, OperatingMode
+import digi.xbee.packets.base as packets
 import time
+import struct
+
+
+# def main():
+#     zigbee = ZigBeeDevice('/dev/ttyUSB0', 9600)
+
+#     try:
+#         zigbee.open()
+#         data = 'Zigbee node %s sending data' % (zigbee.get_node_id())
+#         data = data.encode('utf-8')
+#         rssi_raw = zigbee.get_parameter("DB")
+#         rssi_val = struct.unpack('=B', rssi_raw)
+#         print(rssi_val)
+        #packet_receive = zigbee.send_data_broadcast(data)
+        #data_packet = packets.XBeePacket()
+        #packet = data_packet.create_packet(data, OperatingMode.API_MODE)
+        #packet_receive = zigbee.send_packet(packet)
 
 xbee = ZigBeeDevice("/dev/ttyUSB0", 9600)
 
 try:
     xbee.open()
-    xbee
     print("This Node ID: ", xbee.get_node_id())
     print("Is Remote: ", xbee.is_remote())
     print("Power Level: ", xbee.get_power_level())
@@ -40,15 +57,22 @@ try:
 
     nodes = xnet.get_devices()
     
-    for device in devices:
-        print("Devices found: %s" % device)
+    data = 'Zigbee node %s sending data' % (zigbee.get_node_id())
 
-    def get_packet_recieved_callback(packet):
+    for node in nodes:
+        print("Nodes found: %s" % node)
+        data = data.encode('utf-8')
+        rssi_raw = zigbee.get_parameter('DB')
+        rssi_val = struct.unpack('=B', rssi_raw)
+        print("Node RSSI: %s" % rssi_val)
+
+    def packet_received_callback(packet):
         packet_dict = packet.to_dict()
         api_data = packet_dict[DictKeys.FRAME_SPEC_DATA][DictKeys.API_DATA]
-            
-    def initialize_nodes():
-	for node in nodes:
+        print(api_data)
+    
+    # print(packet_receive)
+    # zigbee.add_packet_received_callback(packet_received_callback)
 		
 		 
 finally:
